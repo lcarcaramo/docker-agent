15fe93130b019d707b973c53c1f2516533c9a5fa
lcarcaramo <72973973+lcarcaramo@users.noreply.github.com>
2020-11-04 09:57:28 -0500

    See if logs remain.

--- s390x/test/11/ubuntu/test.sh
+++ s390x/test/11/ubuntu/test.sh
@@ -49,8 +49,8 @@ suite_end () {
 
 suite_start
         print_test_case "It can run a Jenkins agent:"
-                trap 'docker run -i --rm --name test-jenkins-agent --init "quay.io/ibmz/jenkins-agent:4.3" java -jar /usr/share/jenkins/agent.jar <<< exit' 1
-                # docker logs test-jenkins-agent
+                trap 'docker run -i --name test-jenkins-agent --init "quay.io/ibmz/jenkins-agent:4.3" java -jar /usr/share/jenkins/agent.jar <<< exit' 1
+                docker logs test-jenkins-agent
                 #export TEST_AGENT_LOGS=$(docker logs test-jenkins-agent)
                 # echo $TEST_AGENT_LOGS
                 # docker rm -f test-jenkins-agent
